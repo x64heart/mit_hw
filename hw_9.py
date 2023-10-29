caeb077374cb89058f91aec5b4638edf0ffac8be
x64heart <boggybxxwa@gmail.com>
2023-10-29 15:52:19 +0300

    idk

--- hw_9.py
+++ hw_9.py
@@ -2,6 +2,8 @@
 vowels = 'aeoiu'
 arr = [15, 48, 'hello', 6, 19, 'world']
 counter = 0
+vowels_total = 0
+consonants_total = 0
 for item in arr:
     if type(item) == int:
         if item % 2:
@@ -15,7 +17,10 @@ for item in arr:
         for c in item:
             if c in vowels:
                 vowels_count += 1
+                vowels_total += 1
             else:
                 consonants_count += 1
+                consonants_total += 1
         print(f"{item} , {vowels_count} гласных, {consonants_count} согласных")
     counter += 1
+print(f"Всего гласных :{vowels_total}, всего согласных {consonants_total}")
